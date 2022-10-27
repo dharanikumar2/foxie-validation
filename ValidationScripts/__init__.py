a76a736651b5380a553270b52de6e150acfce190
dharanikumar <dharanikumar@increff.com>
2022-10-27 17:34:44 +0530

    string changed

--- ValidationScripts/__init__.py
+++ ValidationScripts/__init__.py
@@ -37,6 +37,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         #download_file_from_directory("sample_file.csv")
     else:
         return func.HttpResponse(
-             "Please pass a command to execute the functio",
+             "Please pass a command to execute the validation function",
              status_code=200
         )
